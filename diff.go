3e3ed5136da1fbf0ae76c05fe95f06c97ca423c0
Ilya Brin <464157+ilyabrin@users.noreply.github.com>
2021-07-05 06:20:28 +0300

    WIP - with channels (#4)

--- diff.go
+++ diff.go
@@ -8,6 +8,8 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"sync"
+	"time"
 )
 
 /*
@@ -25,6 +27,8 @@ const (
 	MIN_IMAGE_HEIGHT = "1px"
 	MIN_IMAGE_WIDTH  = "1px"
 
+	RESULT_IMAGE_PATH = "./parts/result.png"
+
 	QUAD_SIZE = 100 // 100px
 )
 
@@ -32,9 +36,61 @@ func init() {
 	image.RegisterFormat("png", "png", png.Decode, png.DecodeConfig)
 }
 
-var imageParts1, imageParts2 []image.Image
+// var imageParts1, imageParts2, compared []image.Image
+var compared []image.Image
+
+type Pair struct {
+	Original image.Image
+	Compared image.Image
+}
+
+// pair send to compare
+// compared receives result part (will rendered at final)
+func parallelProcessDifferences(parts chan Pair, w, h int) {
+	// помещаем в канал parts пару Pair{orig, comp}
+	// затем в горутине сравниваем пары и пишем результат в compared
+	// после всеъ проверок и сравнений рендерим все части из compared
+	// imCh := make(chan image.Image)
+
+	len_parts := len(parts)
+
+	var wg sync.WaitGroup
+
+	// TODO: должно быть не так, цикл заменить на горутину и канал(ы)
+	// правильно так: diff проверяет совпадение и помещает в канал результат
+	// как только результат по]вилс] в канале - друга] горутина забирает его в компаред
+	// если все пары обработаны - рендерим картинку и завершаем работу
+	for x := 0; x < len_parts; x++ {
+		wg.Add(1) // TODO: должны параллельно сразу все пары влететь, а не по одной
+		part := <-parts
+		go diff(part.Original, part.Compared, &wg)
+		// time.Sleep(5 * time.Microsecond)
+	}
+	wg.Wait()
+	close(parts)
+
+	newImage := image.NewRGBA(image.Rect(0, 0, w, h))
+	clr, _ := ToRGBA("FFFFFF")
+
+	draw.Draw(newImage, newImage.Bounds(), image.NewUniform(clr), image.Point{}, draw.Src)
+
+	for idx := range compared {
+		draw.Draw(newImage, newImage.Bounds(), compared[idx], image.Point{0, 0}, draw.Src)
+	}
+
+	f, err := os.Create(RESULT_IMAGE_PATH)
+	if err != nil {
+		log.Println(err)
+	}
+	defer f.Close()
+
+	err = png.Encode(f, newImage)
+
+}
 
 func main() {
+
+	start := time.Now()
 
 	numArgs := len(os.Args[1:])
 	if numArgs != 2 {
@@ -106,13 +162,14 @@ func main() {
 	imgOne, _, err := image.Decode(img1)
 	imgTwo, _, err := image.Decode(img2)
 
-	imageParts1 := make([]image.Image, 0, x_iter*y_iter)
-	imageParts2 := make([]image.Image, 0, x_iter*y_iter)
+	// imageParts1 := make([]image.Image, 0, x_iter*y_iter)
+	// imageParts2 := make([]image.Image, 0, x_iter*y_iter)
+
+	chanPairs := make(chan Pair, x_iter*y_iter) // store Pairs for compare
+	// chanPairs := make(chan Pair) // store Pairs for compare
 
 	for y := 0; y < y_iter; y++ {
 		for x := 0; x < x_iter; x++ {
-
-			// TODO: send to chan with parts
 
 			if x == x_iter && x_mod != 0 {
 				x1 -= QUAD_SIZE + x_mod
@@ -126,12 +183,17 @@ func main() {
 			img_part1 := imgOne.(interface {
 				SubImage(r image.Rectangle) image.Image
 			}).SubImage(image.Rect(x0, y0, x1, y1))
-			imageParts1 = append(imageParts1, img_part1)
+			// imageParts1 = append(imageParts1, img_part1)
 
 			img_part2 := imgTwo.(interface {
 				SubImage(r image.Rectangle) image.Image
 			}).SubImage(image.Rect(x0, y0, x1, y1))
-			imageParts2 = append(imageParts2, img_part2)
+			// imageParts2 = append(imageParts2, img_part2)
+
+			chanPairs <- Pair{
+				Original: img_part1,
+				Compared: img_part2,
+			}
 
 			x0 += QUAD_SIZE
 			x1 += QUAD_SIZE
@@ -143,39 +205,23 @@ func main() {
 		y1 += QUAD_SIZE
 	}
 
-	newImage := image.NewRGBA(image.Rect(0, 0, img1_width, img1_height))
-	clr, _ := ToRGBA("FFFFFF")
+	parallelProcessDifferences(chanPairs, img1_width, img1_height)
 
-	draw.Draw(newImage, newImage.Bounds(), image.NewUniform(clr), image.Point{}, draw.Src)
-
-	for idx := range imageParts1 {
-		// imageParts1[idx], err = diff(imageParts1[idx], imageParts2[idx])
-		img, err := diff(imageParts1[idx], imageParts2[idx])
-		if err != nil {
-			log.Fatal(err)
-		}
-		draw.Draw(newImage, newImage.Bounds(), img, image.Point{0, 0}, draw.Src)
-		// draw.Draw(newImage, newImage.Bounds(), imageParts1[idx], image.Point{0, 0}, draw.Src)
-	}
-
-	f, err := os.Create("./parts/result.png")
-	if err != nil {
-		log.Println(err)
-	}
-	defer f.Close()
-
-	err = png.Encode(f, newImage)
+	elapsed := time.Since(start)
+	log.Printf("Diff took %s", elapsed)
 }
 
 // pixel-by-pixel image parts comparison
-func diff(img1, img2 image.Image) (image.Image, error) {
+func diff(img1, img2 image.Image, wg *sync.WaitGroup) { //(image.Image, error) {
+
+	defer wg.Done()
 
 	bounds := img1.Bounds()
 
 	w := bounds.Max.X
 	h := bounds.Max.Y
 
-	// todo: night code concept
+	// TODO: change At to Pix and benchmark it
 	for y := 0; y < h; y++ {
 		for x := 0; x < w; x++ {
 
@@ -189,7 +235,7 @@ func diff(img1, img2 image.Image) (image.Image, error) {
 			}
 		}
 	}
-	return img1, nil
+	compared = append(compared, img1)
 }
 
 func ToRGBA(h string) (color.RGBA, error) {
